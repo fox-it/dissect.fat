0111098ace81c05ea0f877023066718e617e01ab
Schamper <1254028+Schamper@users.noreply.github.com>
2024-11-01 15:01:33 +0100

    Subclass exceptions from standard library exceptions

--- dissect/fat/exceptions.py
+++ dissect/fat/exceptions.py
@@ -30,9 +30,13 @@ class InvalidDirectoryError(Error):
     pass
 
 
-class FileNotFoundError(Error):
+class FileNotFoundError(Error, FileNotFoundError):
     pass
 
 
-class NotADirectoryError(Error):
+class IsADirectoryError(Error, IsADirectoryError):
+    pass
+
+
+class NotADirectoryError(Error, NotADirectoryError):
     pass
